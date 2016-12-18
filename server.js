4c4178090bcc71f345a0958882ed378b83b4b11d
时尽 <xingchen.sxc@alibaba-inc.com>
2016-12-18 23:09:14 +0800

    ~

--- server.js
+++ server.js
@@ -2,7 +2,19 @@ var koa = require('koa')
 
 var app = koa();
 
+
+var views = require('koa-view');
+
+app.use(views(__dirname + '/dist'));
 app.use(require('koa-static')(__dirname + '/dist'));
 
+// app.use(function*(){
+	// app.use(function *(){
+	  // this.body = 'Hello World';
+	// });
+// })
+app.use(function* (next) {
+  yield this.render('index');
+})
 
-app.listen(8080);
+app.listen(3001);
