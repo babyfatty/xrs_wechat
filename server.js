7fcf0e2b9a622da98c3b2885ebc66bc4d722fdee
babyfat <babyfat@babyfatdeMacBook-Pro.local>
2016-12-27 14:31:56 +0800

    ~

--- server.js
+++ server.js
@@ -4,15 +4,28 @@ var app = koa();
 
 var views = require('co-views');
 
+var fs = require('fs')
+
+var path = require('path')
+
+var rs = fs.createReadStream(path.join(__dirname, './dist/index.html'))
+
 var render = views(__dirname + '/dist', {
   map: { html: 'swig' }
 });
+
+var index = require.resolve('./dist/index.html')
+
+
 
 app.use(require('koa-static')(__dirname + '/dist'));
 
 
 app.use(function* (next) {
-  this.body = yield render('index');
+	this.type = 'html'
+	this.body = rs
+  // this.body = yield render('index');
+
 })
 
 app.listen(3002);
